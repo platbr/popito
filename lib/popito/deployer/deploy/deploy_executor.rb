0c4aff97eb84590304e8e99ec555dec8421bf84d
Wagner Caixeta <wagner@baladapp.com.br>
2024-04-01 19:16:42 -0300

    Ajustes para conseguir gerar o docker-compose

--- lib/popito/deployer/deploy/deploy_executor.rb
+++ lib/popito/deployer/deploy/deploy_executor.rb
@@ -16,5 +16,24 @@ module Popito
       puts 'Applying generated files...'
       system "kubectl apply -f #{@deploy_files_dir}", exception: true
     end
+
+    def push_deployed_tag
+      yaml_build_config["build"].each do |build|
+        tag = build["tags"].first
+        puts "Pushing #{build["image"]}:#{tag} to #{build["image"]}:popito-#{config_payload.build_config[:ENVIRONMENT]}"
+        system "docker image tag #{build["image"]}:#{tag} #{build["image"]}:popito-#{config_payload.build_config[:ENVIRONMENT]}", exception: true
+        system "docker push #{build["image"]}:popito-#{config_payload.build_config['ENVIRONMENT']}", exception: true
+      end
+    end
+
+    private
+    
+    def yaml_build_config
+      @yaml_config ||= load_build_yaml
+    end
+
+    def load_build_yaml
+      YAML.safe_load(File.read("#{config_payload.build_path}/build.yaml"))
+    end
   end
 end
